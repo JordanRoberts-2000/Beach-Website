cc5f06dda9e1ca90450aa28519a76ce8e1eea178
Jordan <jordanroberts333@icloud.com>
2023-05-03 17:06:39 +0100

    Initil commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    images: {
+      // loader: 'custom',
+      // loaderFile: './Utils/CustomImageLoader.ts',
+      // deviceSizes: [640, 768, 1024],
+      domains: ['res.cloudinary.com']
+    },
     appDir: true,
   },
 }
