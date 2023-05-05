2d833e38bb9aa70a0c2b436ccdf4454c662e4dec
Jordan <jordanroberts333@icloud.com>
2023-05-05 23:22:01 +0100

    scroll lock improvements when animating in and out of category, exit button added

--- app/Components/Category/Category.tsx
+++ app/Components/Category/Category.tsx
@@ -23,37 +23,50 @@ type Props = {
 const Category = ({children, imageUrl, title, subtitle, priceOptions}: Props) => {
     let categoryRef = useRef<HTMLLIElement>(null)
     let scrollUpRef = useRef<HTMLDivElement>(null)
+    let categoryScrollRef = useRef<HTMLDivElement>(null)
     const { categoryClicked } = useStore()
     const [active, setActive] = useState(false)
     const [priceActive, setPriceActive] = useState(true)
-    const handleClick = (e:any) => {
-        if(categoryClicked !== ""){
-            useStore.setState(() => ({categoryClicked: ""}))
-        }else{
-            useStore.setState(() => ({categoryClicked: title}))
-        }
+    const handleSelected = () => {
+        if(active)return
+        console.log('entering')
+        useStore.setState(() => ({
+            categoryClicked: title,
+            bodyLocked: true
+        }))
         setTimeout(() => {
             categoryRef.current!.scrollIntoView({ behavior: "smooth"})
-            if(priceActive)setPriceActive((prev) => !prev)
-            if(active)setActive((prev) => !prev)
+            setPriceActive(false)
             setTimeout(() => {
-                if(!active)setActive((prev) => !prev)
-                if(!priceActive)setPriceActive((prev) => !prev)
-                if(active){
-                    scrollUpRef.current!.scrollIntoView({ behavior: "smooth"})
-                    useStore.setState((set: any) => ({bodyLocked: !set.bodyLocked}))
-                }
-            },400)
+                setActive(true)
+            }, 400)
         },10)
-        
+    }
+    const handleExit = () => {
+        if(!active)return
+        categoryScrollRef.current!.scrollTo({ top: 0, behavior: 'smooth' })
+        setTimeout(() => {
+            useStore.setState(() => ({categoryClicked: ""}))
+            setActive(false)
+            setTimeout(() => {
+                setPriceActive(true)
+                scrollUpRef.current!.scrollIntoView({ behavior: "smooth"})
+                setTimeout(() => {
+                    useStore.setState((set: any) => ({bodyLocked: false}))
+                }, 200)
+            }, 400)
+        }, 300)
     }
     return (
-        <li ref={categoryRef} className={`grid bg-white lg:grid-cols-2 lg:grid-rows-2 relative`} onClick={(e) => handleClick(e)}>
+        <li ref={categoryRef} className={`grid bg-white lg:grid-cols-2 lg:grid-rows-2 relative`} onClick={() => handleSelected()}>
             <div ref={scrollUpRef} className="absolute opacity-0 pointer-events-none top-[-50px] h-[1px] w-full"></div>
-            <div className={`${!active ? "overflow-hidden aspect-[3/2] w-[95%]" : "w-full aspect-auto h-[100lvh] overflow-scroll"} mx-auto
+            <button className={`bg-black ${active ? "opacity-100 pointer-events-auto  duration-300" : "opacity-0 pointer-events-none"} p-2 rounded-full absolute bottom-0 right-0 m-4 z-40`} onClick={() => handleExit()}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="white" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
+            </button>
+            <div ref={categoryScrollRef} className={`${!active ? "overflow-hidden aspect-[3/2] w-[95%]" : "w-full aspect-auto h-[100lvh] overflow-scroll"} mx-auto
             ${categoryClicked !== title && categoryClicked !== '' ? 'opacity-0 duration-200' : 'opacity-100 duration-500'}`}>
                 {/* Image/Gallery */}
-                <div className="w-[100%] lg:w-full aspect-[3/2] lg:aspect-auto lg:h-[50vh] mx-auto relative mb-4">
+                <div className="w-[100%] lg:w-full aspect-[3/2] lg:aspect-auto lg:h-[50vh] mx-auto relative mb-4" onClick={() => handleExit()}>
                     <CategoryImage imageUrl={imageUrl} active={active}/>
                     <div className="absolute top-0 w-full h-full bg-white/40"></div>
                     <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col text-center">
