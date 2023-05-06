0c1b31173a98f6b1188fa0f0c5146478cca0b837
Jordan <jordanroberts333@icloud.com>
2023-05-06 02:17:23 +0100

    Mobile navbar menu added

--- app/Components/Header/HamburgerMenu.tsx
+++ app/Components/Header/HamburgerMenu.tsx
@@ -4,9 +4,9 @@ import { motion } from "framer-motion"
 import { useState } from "react"
 
 const variants = { 
-    oneActive: { width: ['2rem'], rotate: 45, y: 10 }, oneInactive: { width: '2rem', },
+    oneActive: { width: ['1.5rem'], rotate: 45, y: 10 }, oneInactive: { width: '2rem', },
     twoActive: { width: ['2rem', '0rem'] }, twoInactive: { width: '1.25rem' },
-    threeActive: { width: '2rem', rotate: -45, y:-10 }, threeInactive: { width: '1.25rem' }
+    threeActive: { width: '1.5rem', rotate: -45, y:-10 }, threeInactive: { width: '1.25rem' }
 }
 
 const HamburgerMenu = () => {
@@ -15,11 +15,24 @@ const HamburgerMenu = () => {
         setActive((prev) => !prev)
     }
     return (
-        <div className='flex flex-col gap-2 ml-2 lg:hidden items-center' onClick={() => handleClick()}>
-            <motion.div animate={active ? "oneActive" : "oneInactive"} variants={variants} className='h-[2px] bg-black w-8'></motion.div>
-            <motion.div animate={active ? "twoActive" : "twoInactive"} variants={variants} className={`h-[2px] bg-black w-5 ${!active && 'mr-3'}`}></motion.div>
-            <motion.div animate={active ? "threeActive" : "threeInactive"} variants={variants} className={`h-[2px] bg-black w-5 ${!active && 'mr-3'}`}></motion.div>
-        </div>
+        <>
+            <div className='flex flex-col gap-2 ml-2 lg:hidden items-center z-10' onClick={() => handleClick()}>
+                <motion.div animate={active ? "oneActive" : "oneInactive"} variants={variants} className='h-[2px] bg-black w-8'></motion.div>
+                <motion.div animate={active ? "twoActive" : "twoInactive"} variants={variants} className={`h-[2px] bg-black w-5 ${!active && 'mr-3'}`}></motion.div>
+                <motion.div animate={active ? "threeActive" : "threeInactive"} variants={variants} className={`h-[2px] bg-black w-5 ${!active && 'mr-3'}`}></motion.div>
+            </div>
+            <nav className={`${!active ? "opacity-0 pointer-events-none" : "opacity-100 pointer-events-auto"} lg:opacity-100 lg:relative lg:bg-white/0 lg:h-auto 
+                            lg:w-auto top-0 fixed h-[100lvh] w-[100%] left-0 bg-white/80 duration-300 lg:ml-auto flex justify-center`}>
+                <ul className='flex gap-12 mt-20 lg:mr-8 font-bold flex-col text-center text-3xl lg:text-base lg:flex-row lg:mt-0'>
+                    <li className="border-b-2 border-black lg:border-none">FISHING</li>
+                    <li className="border-b-2 border-black lg:border-none">VOLLYBALL</li>
+                    <li className="border-b-2 border-black lg:border-none">PARASAILING</li>
+                    <li className="border-b-2 border-black lg:border-none">JET SKIS</li>
+                    <li className="border-b-2 border-black lg:border-none">SURFING</li>
+                    <li className="border-b-2 border-black lg:border-none">SWIMMING</li>
+                </ul>
+            </nav>
+        </>
     )
 }
 
--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -6,16 +6,7 @@ const Header = () => {
     <header className='fixed flex z-50 top-0 w-full py-4 items-center backdrop-blur-sm'>
         {/* Hamburger Menu */}
         <HamburgerMenu/>
-        <nav className='ml-auto hidden lg:flex'>
-            <ul className='flex gap-12 mr-8 font-bold'>
-                <li>FISHING</li>
-                <li>VOLLYBALL</li>
-                <li>PARASAILING</li>
-                <li>JET SKIS</li>
-                <li>SURFING</li>
-                <li>SWIMMING</li>
-            </ul>
-        </nav>
+        
         <CallToAction/>
     </header>
   )
