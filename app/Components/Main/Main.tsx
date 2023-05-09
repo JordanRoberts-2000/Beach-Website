f813167448b9c8a2ee2cb4e70d64ae8d5de4013f
Jordan <jordanroberts333@icloud.com>
2023-05-09 17:34:42 +0100

    homepage ui variation

--- app/Components/Main/Main.tsx
+++ app/Components/Main/Main.tsx
@@ -5,9 +5,9 @@ import MainResize from "./MainResize"
 const Main = () => {
   return (
     <MainResize>
-        <div className="relative h-[35vh] lg:h-full w-full flex mb-2">
+        <div className="relative h-[35vh] lg:h-full w-full flex flex-col">
             <HomeImage/>
-            <div className="fadeWhite translate-y-[2px] lg:hidden w-full h-6 z-10 mt-auto"></div>
+            <div className="fadeWhite translate-y-[2px] lg:hidden h-6 z-10 absolute bottom-0 w-full"></div>
             <div className="absolute bottom-0 left-[50%] translate-x-[-50%] w-[90%]">
                 <RiseFade duration={.6} delay={1} awaitPreload={true}>
                     <div className="flex mt-auto gap-12 z-30 mb-16 ">
@@ -22,19 +22,9 @@ const Main = () => {
                     </div>
                 </RiseFade>
             </div>
-        </div>
-        {/* <span className="text-2xl font-[600] ml-4">About</span>
-        <p className="ml-4 mr-4 mb-8 font-bold lg:hidden">
-            We are dedicated to providing you with
-            the best experience possible when planning
-            your next trip to the beach. We offers a wide
-            range of activities that will make your beach
-            adventure unforgettable.
-        </p> */}
-        {/* Social Links */}
-        <div className="flex flex-col lg:absolute lg:bottom-0 lg:right-0 lg:mr-12 lg:mb-16">
+            <div className="flex flex-col lg:absolute lg:bottom-0 lg:right-0 lg:mr-12 lg:mb-16">
             {/* <button className="w-fit shadow-lg border-2 border-black lg:bg-black lg:text-white rounded-sm font-bold px-4 mb-4 py-1 ml-auto mr-2 lg:rounded-none lg:text-3xl lg:font-playfairDisplay lg:font-[600]">See Our Reviews</button> */}
-            <div className="flex pr-4 ml-4 items-center lg:flex-col-reverse lg:items-start lg:ml-0">
+            <div className="flex pr-4 ml-4 items-center lg:flex-col-reverse lg:items-start lg:ml-0 z-30 pb-4 absolute bottom-0 w-full px-2 left-0 translate-x-[-.5rem]">
                 <ul className="flex gap-8 lg:gap-12 items-center">
                     <li>
                         <svg className="h-5 lg:h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/></svg>
@@ -50,6 +40,17 @@ const Main = () => {
                 <span className="text-xl font-[600] font-playfairDisplay lg:mb-4 lg:text-3xl lg:bg-black lg:text-white lg:px-4 lg:py-1">Follow Us</span>
             </div>
         </div>
+        </div>
+        {/* <span className="text-2xl font-[600] ml-4">About</span>
+        <p className="ml-4 mr-4 mb-8 font-bold lg:hidden">
+            We are dedicated to providing you with
+            the best experience possible when planning
+            your next trip to the beach. We offers a wide
+            range of activities that will make your beach
+            adventure unforgettable.
+        </p> */}
+        {/* Social Links */}
+        
     </MainResize>
   )
 }
