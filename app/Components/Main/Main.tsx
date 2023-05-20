a7b4294791defeed46af79351d503a4c834b2d39
Jordan <jordanroberts333@icloud.com>
2023-05-20 09:56:06 +0100

    intro paragraph added to home page

--- app/Components/Main/Main.tsx
+++ app/Components/Main/Main.tsx
@@ -14,7 +14,7 @@ const Main = async () => {
                 <div className="fadeWhite translate-y-[2px] h-6 z-10 absolute bottom-0 w-full"></div>
                 <div className="absolute bottom-0 left-[50%] translate-x-[-50%] w-[90%] lg:static mt-auto lg:translate-x-0 lg:w-fit lg:ml-[65%]">
                     <RiseFade duration={.6} delay={.6} awaitPreload={true}>
-                        <div className="flex mt-auto gap-12 z-30 mb-16 lg:mb-2">
+                        <div className="flex mt-auto gap-12 z-30 mb-12 lg:mb-2">
                             <Link href={'bookings'} className="border-2 font-sm shadow-lg py-1 text-center border-black backdrop-blur-sm flex-1 rounded-sm px-4 font-bold whitespace-nowrap
                                                     lg:hidden">
                                 Book now
@@ -23,7 +23,7 @@ const Main = async () => {
                         </div>
                     </RiseFade>
                 </div>
-                <RiseFade duration={.6} delay={1} awaitPreload={true} className={'flex items-center lg:items-start z-20 mb-4 lg:mb-16 lg:flex-col-reverse lg:ml-[65%]'} wrapperClassName="mt-auto lg:h-fit lg:mt-0">
+                {/* <RiseFade duration={.6} delay={1} awaitPreload={true} className={'flex items-center lg:items-start z-20 mb-4 lg:mb-16 lg:flex-col-reverse lg:ml-[65%]'} wrapperClassName="mt-auto lg:h-fit lg:mt-0">
                     <ul className="flex gap-8 lg:gap-16 items-center ml-2 z-20">
                         <li>
                             <svg className="h-5 lg:h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/></svg>
@@ -37,7 +37,7 @@ const Main = async () => {
                     </ul>
                     <div className="h-[2px] flex-1 bg-black mx-4 z-20 lg:hidden"></div>
                     <span className="text-2xl font-[800] italic font-playfairDisplay lg:mb-4 lg:text-4xl lg:bg-black lg:text-white mr-2 backdrop-blur-sm lg:pl-4 lg:py-2 lg:pr-8">Follow Us</span>
-                </RiseFade>
+                </RiseFade> */}
             </div>
         </MainResize>
     )
--- utils/components/Animation/Fade.tsx
+++ utils/components/Animation/Fade.tsx
@@ -10,7 +10,7 @@ type FadeProps = React.HTMLAttributes<HTMLDivElement> & {
 const Fade = ({children, className}: FadeProps) => {
     const { categoryClicked } = useStore()
     return (
-        <motion.div initial={{opacity: 0}} animate={categoryClicked !== "" ? "visible" : "hidden"} className={`flex flex-col ${className}`}
+        <motion.div initial={{opacity: 0}} animate={categoryClicked !== "" ? "visible" : "visible"} className={`flex flex-col ${className}`}
                     variants={{ visible: { opacity: 1, transition: {duration: .3, delay: .6}}, hidden: { opacity: 0, transition: {duration: 0.1}} }}>
             {children}
         </motion.div>
